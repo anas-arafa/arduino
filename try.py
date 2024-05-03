faa10f2bc9f611482349aaafc95a19f5fed6d613
anas-arafa <anas.prime2011@gmail.com>
2024-05-03 16:57:38 +0300

    fix issue

--- try.py
+++ try.py
@@ -4,9 +4,8 @@ import time
 board = pyfirmata.Arduino('/dev/ttyUSB0')
 # board = pyfirmata.Arduino('/dev/ttyACM0')
 
-
 x = int(input("enter one or zero  "))
-print(time.)
+print(time.time())
 if x==1:
    board.digital[13].write(1)
 
