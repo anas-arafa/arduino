d56eb5b2005c33b7b20b986a3a8b55134479434b
anas-arafa <anas.prime2011@gmail.com>
2024-05-03 17:02:04 +0300

    make a sleep 2 sec

--- try2.py
+++ try2.py
@@ -5,6 +5,6 @@ board = pyfirmata.Arduino('/dev/ttyUSB0')
 
 while True:
     board.digital[13].write(1)
-    time.sleep(1)
+    time.sleep(2)
     board.digital[13].write(0)
-    time.sleep(1)
+    time.sleep(2)
